82b988b5eb7f52323737d3bc1c7e680f18e988c4
Hoàng Ak <96946712+hoangakccm@users.noreply.github.com>
2023-04-22 19:43:10 +0700

    HoangAk Commit

--- facecap.py
+++ facecap.py
@@ -1,45 +1,53 @@
 import cv2
 import os
 
-cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video width
-cam.set(4, 480) # set video height
 
-face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+def Face_Cap(name, face_id):
 
-# For each person, enter one numeric face id
-face_id = input('Nhap id: ')
-name = input("Nhap ten: ")
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640) # set video width
+    cam.set(4, 480) # set video height
 
-print("\n [INFO] Initializing face capture. Look the camera and wait ...")
-# Initialize individual sampling face count
-count = 0
+    face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-while(True):
+    # For each person, enter one numeric face id
+    # face_id = input('Nhap id: ')
+    # name = input("Nhap ten: ")
+    # name = get_name()
+    # face_id = get_face_id()
+    # print("Tên: " ,name)
+    # print("IDD: ", face_id)
 
-    ret, img = cam.read()
-    img = cv2.flip(img, 1) # flip video image vertically
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
 
-    for (x,y,w,h) in faces:
+    print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+    # Initialize individual sampling face count
+    count = 0
 
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
-        count += 1
+    while(True):
 
-        # Save the captured image into the datasets folder
-        cv2.imwrite("dataset/"+ str(name)+ '.' + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
-        if not os.path.exists("dataset"):
-            os.makedirs("dataset")
-        cv2.imshow('image', img)
+        ret, img = cam.read()
+        img = cv2.flip(img, 1) # flip video image vertically
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
 
-    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
-    if k == 27:
-        break
-    elif count >= 200: # Take 200 face sample and stop video
-         break
+        for (x,y,w,h) in faces:
 
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup stuff")
-cam.release()
-cv2.destroyAllWindows()
+            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+            count += 1
+
+            # Save the captured image into the datasets folder
+            cv2.imwrite("dataset/"+ str(name)+ '.' + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+            if not os.path.exists("dataset"):
+                os.makedirs("dataset")
+            cv2.imshow('image', img)
+
+        k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+        if k == 27:
+            break
+        elif count >= 200: # Take 200 face sample and stop video
+             break
+
+    # Do a bit of cleanup
+    print("\n [INFO] Exiting Program and cleanup stuff")
+    cam.release()
+    cv2.destroyAllWindows()
