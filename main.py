f0514612cbef2d9edb14a71b65fdf754f350cc53
Hoàng Ak <96946712+hoangakccm@users.noreply.github.com>
2023-04-22 19:38:32 +0700

    Add files via upload
    HoangAk Commit

--- main.py
+++ main.py
@@ -2,68 +2,81 @@ import cv2
 import numpy as np
 import os
 
+def TEST():
+    folder_path = "dataset"
 
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trainer/trainer.yml')
-cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
+    # Tạo một mảng để lưu tên các tệp tin
+    file_names = [None]
 
-font = cv2.FONT_HERSHEY_SIMPLEX
+    # Lặp qua các tệp tin trong thư mục và thêm tên của từng tệp tin vào mảng
+    for i, file_name in enumerate(os.listdir(folder_path)):
+        # Nếu số thứ tự của tệp tin là bội số của 100, thì thêm tên của tệp tin vào mảng
+        if i % 200 == 0:
+            file_names.append(file_name.split(".")[0])
 
-# iniciate id counter
-id = 0
+    # In tên các tệp tin trong mảng
+    print(file_names)
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read('trainer/trainer.yml')
+    cascadePath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(cascadePath);
 
-# names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'TranVanPhuc']
+    font = cv2.FONT_HERSHEY_SIMPLEX
 
-# Initialize and start realtime video capture
-cam = cv2.VideoCapture(0)
-cam.set(3, 640)  # set video widht
-cam.set(4, 480)  # set video height
+    # iniciate id counter
+    id = 0
 
-# Define min window size to be recognized as a face
-minW = 0.1 * cam.get(3)
-minH = 0.1 * cam.get(4)
+    # names related to ids: example ==> Marcelo: id=1,  etc
+    # names = ['None',"Hoang1","hoang2", "hoang3"]
 
-while True:
+    # Initialize and start realtime video capture
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640)  # set video widht
+    cam.set(4, 480)  # set video height
 
-    ret, img = cam.read()
-    img = cv2.flip(img, 1) # Flip vertically
+    # Define min window size to be recognized as a face
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    while True:
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(int(minW), int(minH)),
-    )
+        ret, img = cam.read()
+        img = cv2.flip(img, 1) # Flip vertically
 
-    for (x, y, w, h) in faces:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(int(minW), int(minH)),
+        )
 
-        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+        for (x, y, w, h) in faces:
 
-        # Check if confidence is less them 100 ==> "0" is perfect match
-        if (confidence < 100):
-            id = names[id]
-            confidence = "  {0}%".format(round(confidence),2)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        else:
-            id = "unknown"
-            confidence = "  {0}%".format(round(confidence),2)
+            id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 
-        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
-        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+            # Check if confidence is less them 100 ==> "0" is perfect match
+            if (confidence < 100):
+                id = file_names[id]
+                confidence = "  {0}%".format(round(confidence),2)
 
-    cv2.imshow('camera', img)
+            else:
+                id = "unknown"
+                confidence = "  {0}%".format(round(confidence),2)
 
-    k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
-    if k == 27:
-        break
+            cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+            cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
 
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup stuff")
-cam.release()
-cv2.destroyAllWindows()
+        cv2.imshow('camera', img)
+
+        k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
+        if k == 27:
+            break
+
+    # Do a bit of cleanup
+    print("\n [INFO] Exiting Program and cleanup stuff")
+    cam.release()
+    cv2.destroyAllWindows()
